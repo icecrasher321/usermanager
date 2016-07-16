f30cb1cdf5082c7a3dd8a87b51b3ead76ebabae4
icecrasher321 <vikhyathvikku@gmail.com>
2016-07-16 15:54:35 +0530

    Add test data for easier testing

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-  fdb "github.com/icecrasher321/usermanage" // fake database (.txt file)
+	fdb "github.com/icecrasher321/usermanage"
 )
 
- func main() {
-   fdb.CreateRecord("libero101", "Vikhyath", "Mondreti", 16, []int{9741719089}, []string{"x@y.com", "y@z.com"})
+func main() {
+	protocol()
+	// fdb.DeleteRecord("icecaft")
+}
+
+func protocol() {
+	fdb.DbReset()
+	fdb.CreateRecord("iecraf", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085, 9741719043}, []string{"1@2.com"})
+	fdb.CreateRecord("icecrat", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
+	fdb.CreateRecord("icecrft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("icecaft", "lunatic", "1000", 3, []int{9741719086, 9741719085, 9741717865}, []string{"locker@hotmail.com", "3@4.com"})
+
+	fdb.CreateRecord("iceraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
+	fdb.CreateRecord("iccraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("iecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
+	fdb.CreateRecord("cecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com", "4@5.com"})
+	fdb.CreateRecord("icecft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("icraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("ecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("icet", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
+	fdb.CreateRecord("iccrat", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
 }
