a86cdf871d0323b43645364a8a98bf7899801fb6
icecrasher321 <vikhyathvikku@gmail.com>
2016-08-03 12:42:08 +0530

    Create seed function to generate records
    Use github.com/icrowley/fake library for text generation

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	fdb "github.com/icecrasher321/usermanage"
+	"github.com/icrowley/fake"
+	"strconv"
+	"fmt"
 )
 
 func main() {
-	//protocol()
-	//fdb.FetchUserUI("icet")
+	//fdb.DbReset()
+	//seed()
+	//fdb.FetchUserUI("6Jacobs39992")
 	//fdb.UpdateRecordUI("iccrat", "flunatic3", "100", 64, []int{9741712134, 9741719085, 9741717865}, []string{"lo@tytytytyt.com", "10000@101.com"})
 	//fdb.DeleteRecord("iccrat")
 	//fdb.CreateRecord("watermeloyyyt", "Space", "X", 43, []int{9741712134, 9741719085, 9741719043}, []string{"elon@musk.com"})
@@ -14,20 +18,11 @@ func main() {
 	//fdb.DbDrop()
 }
 
-func protocol() {
+func seed() { // 10,000 users per minute
 	fdb.DbReset()
-	fdb.CreateRecordUI("iecraf", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085, 9741719043}, []string{"1@2.com"})
-	fdb.CreateRecord("icecrat", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
-	fdb.CreateRecord("icecrft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("icecaft", "lunatic", "1000", 3, []int{9741719086, 9741719085, 9741717865}, []string{"locker@hotmail.com", "3@4.com"})
-
-	fdb.CreateRecord("iceraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
-	fdb.CreateRecord("iccraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("iecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
-	fdb.CreateRecord("cecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com", "4@5.com"})
-	fdb.CreateRecord("icecft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("icraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("ecraft", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("icet", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com"})
-	fdb.CreateRecord("iccrat", "Vikhyath", "Mondreti", 13, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
+	fmt.Println("Seeding database ......... (This could take a while)")
+	for i := 1; i < 100000; i++ {
+		username := fmt.Sprintf(fake.UserName() + strconv.Itoa(i))
+		fdb.CreateRecord(username, fake.FirstName(),  fake.LastName(), i, []int{9741719086, 9741719085}, []string{"1@2.com", "3@4.com"})
+	}
 }
